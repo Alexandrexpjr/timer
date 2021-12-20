b328a65a8436e3100e5e01b81376de6c15431e38
alexandrexpj <alexandrexpjr@hotmail.com>
2021-12-20 07:50:32 -0300

    add link do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p> &#169; Todos os direitos reservados, Alexandre Pimentel</p>
+      <p> &#169; Todos os direitos reservados, <a href="https://www.linkedin.com/in/alexandrepimenteljr/">Alexandre Pimentel</a></p>
     </footer>
   );
 }
